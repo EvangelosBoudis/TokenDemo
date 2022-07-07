3b1e37869a6ca7ee33a80041e492160495fd4f0f
EvangelosBoudis <evangelos.boudis@gmail.com>
2022-07-07 13:25:01 +0300

    Code spaces update

--- src/main/kotlin/com/example/token/security/utils/AuthenticationToken.kt
+++ src/main/kotlin/com/example/token/security/utils/AuthenticationToken.kt
@@ -10,10 +10,16 @@ data class AuthenticationToken(
     private val details: Map<String, Any> = emptyMap()
 ) : Authentication {
     override fun getName() = username
+
     override fun getAuthorities() = authorities.toMutableList()
+
     override fun getCredentials() = password
+
     override fun getDetails() = details
+
     override fun getPrincipal() = username
+
     override fun isAuthenticated() = authorities.isNotEmpty()
+
     override fun setAuthenticated(isAuthenticated: Boolean) {}
 }
