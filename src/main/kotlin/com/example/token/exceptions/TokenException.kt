b6a53b1cb8105fcad2e61fb31699810cc67b901d
EvangelosBoudis <evangelos.boudis@gmail.com>
2022-07-09 01:51:34 +0300

    Directory update

--- src/main/kotlin/com/example/token/exceptions/TokenException.kt
+++ src/main/kotlin/com/example/token/exceptions/TokenException.kt
@@ -1,4 +1,4 @@
-package com.example.token.security.utils
+package com.example.token.exceptions
 
 class TokenException(message: String?) : Exception(message) {
     constructor(throwable: Throwable) : this(throwable.message)
