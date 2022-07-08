51ae0425bfb659663eb0284ed22af11d6958fc82
EvangelosBoudis <evangelos.boudis@gmail.com>
2022-07-09 01:50:04 +0300

    Removed manually response handling

--- src/main/kotlin/com/example/token/security/filters/TokenVerificationFilter.kt
+++ src/main/kotlin/com/example/token/security/filters/TokenVerificationFilter.kt
@@ -1,14 +1,14 @@
 package com.example.token.security.filters
 
-import com.example.token.security.utils.TokenException
 import com.example.token.security.managers.TokenManager
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.example.token.exceptions.TokenException
 import org.springframework.http.HttpHeaders.AUTHORIZATION
-import org.springframework.http.HttpStatus.FORBIDDEN
-import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.util.AntPathMatcher
 import org.springframework.web.filter.OncePerRequestFilter
+import java.io.IOException
 import javax.servlet.FilterChain
+import javax.servlet.ServletException
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -16,25 +16,25 @@ class TokenVerificationFilter(
     private val tokenManager: TokenManager
 ) : OncePerRequestFilter() {
 
+    @Throws(ServletException::class, IOException::class)
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
         val authorizationHeader = request.getHeader(AUTHORIZATION)
-        try {
-            val decodedToken = tokenManager.decodedTokenFromHeader(authorizationHeader)
-            SecurityContextHolder.getContext().authentication = decodedToken.asAuthenticationToken()
-        } catch (e: TokenException) {
-            response.status = FORBIDDEN.value()
-            response.contentType = APPLICATION_JSON_VALUE
-            ObjectMapper().writeValue(
-                response.outputStream,
-                mapOf("error_message" to e.message)
-            )
-            return
-        }
+        if (authorizationHeader.isNullOrBlank()) throw TokenException("Authorization Header not provided")
+
+        SecurityContextHolder.getContext().authentication = tokenManager
+            .decodedTokenFromHeader(authorizationHeader)
+            .asAuthenticationToken()
+
         filterChain.doFilter(request, response)
+    }
+
+    @Throws(ServletException::class)
+    override fun shouldNotFilter(request: HttpServletRequest): Boolean {
+        return AntPathMatcher().match("/auth/**", request.servletPath)
     }
 
 }
