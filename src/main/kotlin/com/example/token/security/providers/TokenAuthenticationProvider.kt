1f0e34a578b5225b0c0a2c2fd1a01293273f7003
EvangelosBoudis <evangelos.boudis@gmail.com>
2022-07-09 01:47:51 +0300

    Optional user handling

--- src/main/kotlin/com/example/token/security/providers/TokenAuthenticationProvider.kt
+++ src/main/kotlin/com/example/token/security/providers/TokenAuthenticationProvider.kt
@@ -1,27 +1,30 @@
 package com.example.token.security.providers
 
-import com.example.token.repo.UserRepo
+import com.example.token.repositories.UserRepository
 import com.example.token.security.utils.AuthenticationToken
 import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.AuthenticationException
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
 
 @Component
 class TokenAuthenticationProvider(
-    private val userRepo: UserRepo,
+    private val userRepo: UserRepository,
     private val passwordEncoder: PasswordEncoder
 ) : AuthenticationProvider {
 
+    @Throws(AuthenticationException::class)
     override fun authenticate(authentication: Authentication): Authentication {
-        val user = userRepo.findByUsername(authentication.name)
-        val passMatches = passwordEncoder.matches(
-            authentication.credentials.toString(),
-            user.password
-        )
-        return if (passMatches) user.asAuthenticationToken()
-        else throw BadCredentialsException("Database authentication failed")
+        userRepo.findByUsername(authentication.name)?.let { user ->
+            val passMatches = passwordEncoder.matches(
+                authentication.credentials.toString(),
+                user.password
+            )
+            if (passMatches) return user.asAuthenticationToken()
+        }
+        throw BadCredentialsException("Invalid username or password")
     }
 
     override fun supports(authentication: Class<*>) =
