2aa91885845ddda51249a145d5ab18aa82ec29f2
EvangelosBoudis <evangelos.boudis@gmail.com>
2022-07-09 01:44:19 +0300

    Authentication filter removed & exception filter handler added

---
+++ src/main/kotlin/com/example/token/security/config/FiltersConfig.kt
@@ -0,0 +1,26 @@
+package com.example.token.security.config
+
+import com.example.token.security.filters.ExceptionHandlerFilter
+import com.example.token.security.filters.TokenVerificationFilter
+import com.example.token.security.managers.TokenManager
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer
+import org.springframework.web.filter.CorsFilter
+import org.springframework.web.servlet.HandlerExceptionResolver
+
+class FiltersConfig(
+    private val tokenManager: TokenManager,
+    private val resolver: HandlerExceptionResolver
+) : AbstractHttpConfigurer<FiltersConfig, HttpSecurity>() {
+
+    override fun configure(http: HttpSecurity) {
+        http.addFilterBefore(ExceptionHandlerFilter(resolver), CorsFilter::class.java)
+            .addFilterAfter(TokenVerificationFilter(tokenManager), ExceptionHandlerFilter::class.java)
+
+        /*http.addFilterBefore(ExceptionHandlerFilter(resolver), LogoutFilter::class.java)
+            .addFilterBefore(TokenAuthenticationFilter(tokenManager, http.getSharedObject(AuthenticationManager::class.java)), UsernamePasswordAuthenticationFilter::class.java)
+            .addFilterAfter(TokenVerificationFilter(tokenManager), TokenAuthenticationFilter::class.java)*/
+
+    }
+
+}
