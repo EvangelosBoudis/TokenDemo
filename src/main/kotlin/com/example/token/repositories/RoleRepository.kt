acd0565b9635a65d0bf660970e89fdc2d786a239
EvangelosBoudis <evangelos.boudis@gmail.com>
2022-07-09 01:50:26 +0300

    Name update

---
+++ src/main/kotlin/com/example/token/repositories/RoleRepository.kt
@@ -0,0 +1,10 @@
+package com.example.token.repositories
+
+import com.example.token.domain.data.RoleData
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface RoleRepository : JpaRepository<RoleData, Long> {
+
+    fun findByName(name: String): RoleData
+
+}
